63f47823b7cfd3df691d99fc84b191fb3ee04752
mdm106 <mads.mcmahon@googlemail.com>
2020-06-14 19:19:15 +0100

    Shuffle function added to reducer so that words are shuffled randomly each time go button is pressed

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,11 +1,30 @@
 import initial from './initial';
 
+// below shuffle function obtained from https://github.com/coolaj86/knuth-shuffle
+const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 const timeOn = (state, {words}) => {
     return {
         ...state,
         timerActive: true,
-        words: words.map((val, i) => val.word),
+        words: shuffle(words.map((val, i) => val.word)),
     }
 }
 
