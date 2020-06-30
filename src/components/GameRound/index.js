4262e068b9ad2033c30b166e695cda9b7a1481d9
mdm106 <mads.mcmahon@googlemail.com>
2020-06-30 21:01:57 +0100

    GetWords is unused so removed from import

--- src/components/GameRound/index.js
+++ src/components/GameRound/index.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 
 import GameRound from "./GameRound";
 
-import { getWords, patchScore } from "../../data/actions/api";
+import { patchScore } from "../../data/actions/api";
 import { changeWord } from "../../data/actions/state";
 // mapStateToProps: maps the current state (from the store)
 // to the props that get passed into the wrapped component
