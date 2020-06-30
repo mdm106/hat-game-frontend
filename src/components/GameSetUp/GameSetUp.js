bbd075992d4d3b4eabdd73628c8f96fdcb0feb69
mdm106 <mads.mcmahon@googlemail.com>
2020-06-30 15:40:07 +0100

    Removing timerTotal question from form' ;

--- src/components/GameSetUp/GameSetUp.js
+++ src/components/GameSetUp/GameSetUp.js
@@ -7,22 +7,13 @@ class GameSetUp extends Component {
     constructor(props) {
         super(props);
 
-        let { timerTotal } = props;
-
         this.state = {
-            timerTotal,
             category: "",
         };
-
-        this.handleSeconds = this.handleSeconds.bind(this);
 
         this.handleCategory = this.handleCategory.bind(this);
 
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSeconds(e) {
-        this.setState({ timerTotal: e.currentTarget.value});
     }
 
     handleCategory(e) {
@@ -40,14 +31,6 @@ class GameSetUp extends Component {
             <>
                 <div className="container">
                     <Form onSubmit={ this.handleSubmit }>
-                        <Form.Group controlId="formSeconds">
-                            <Form.Label>Round time (between 30 and 60 seconds, default is 45 seconds)</Form.Label>
-                            <Form.Control type="number"
-                                          onChange={ this.handleSeconds }
-                                          value={ timerTotal }
-                                          min={30}
-                                          max={60} />
-                        </Form.Group>
                         <Form.Group controlId="exampleForm.ControlSelect1">
                             <Form.Label>Example select</Form.Label>
                             <Form.Control as="select" value={category} onChange={this.handleCategory}>
