f0b2260d38c797ba7fe570cf8b243dc121d99e1f
mdm106 <mads.mcmahon@googlemail.com>
2020-07-05 19:52:56 +0100

    Shuffle function changed from reducer to state action file as it is an impure function

--- src/data/actions/state.js
+++ src/data/actions/state.js
@@ -1,7 +1,28 @@
+// below shuffle function obtained from https://github.com/coolaj86/knuth-shuffle
+const shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 export const setCategory = ( data, category ) => {
     return {
         type: "CATEGORY_SET",
-        words: data,
+        words: shuffle(data.map((val, i) => val.word)),
         category: category,
     };
 };
