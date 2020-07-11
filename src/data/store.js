123a38a734fee5b7782bb94aba62ee04a6f6aa15
mdm106 <mads.mcmahon@googlemail.com>
2020-07-11 12:12:56 +0100

    Local storage installed and added to store

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import persistState from "redux-localstorage";
 
 import initial from './initial';
 import reducer from './reducer';
@@ -9,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initial,
-  composeEnhancers(applyMiddleware(thunk)),
+  composeEnhancers(applyMiddleware(thunk), persistState())
 );
 
 export default store;
