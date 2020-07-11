7e3856afb4270a8fef8b69d81b0ceff61f1e00c4
mdm106 <mads.mcmahon@googlemail.com>
2020-07-11 14:02:10 +0100

    Updates

---
+++ precache-manifest.f7e06fe447a8e185005fae2695d2fe23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54c311068a0d50da41b1620f3fa092a",
+    "url": "/hat-game-frontend/index.html"
+  },
+  {
+    "revision": "a41194bd48988a445ea4",
+    "url": "/hat-game-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4aaeaefe4d1684106518",
+    "url": "/hat-game-frontend/static/js/2.bdd14253.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hat-game-frontend/static/js/2.bdd14253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6f9609f898cc254dab",
+    "url": "/hat-game-frontend/static/js/3.20420590.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/hat-game-frontend/static/js/3.20420590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41194bd48988a445ea4",
+    "url": "/hat-game-frontend/static/js/main.4177351f.chunk.js"
+  },
+  {
+    "revision": "cd920b7f530081637236",
+    "url": "/hat-game-frontend/static/js/runtime-main.13f53933.js"
+  },
+  {
+    "revision": "958974918b3b46a92a98f660da82b0ed",
+    "url": "/hat-game-frontend/static/media/air-horn.95897491.mp3"
+  }
+]);
